c7f8a4a38e7a2770370d286a09f17ed66b2452da
Marek <700231897@fvtc.edu>
2024-02-20 19:50:01 -0600

    Practice

---
+++ site.js
@@ -0,0 +1,37 @@
+
+//alert(localStorage.getItem('test') || 'welcome')
+const form = document.querySelector('form')
+const FormData = JSON.parse(localStorage.getItem('test')) || []
+FormData.forEach(kvp => {
+    const[ name, value ] = kvp
+    const element = form.querySelector(`input[name=${name}]`)
+    if (!element) return
+    element.value = value
+    element.dispatchEvent(new Event('input'))
+})
+
+
+const allInputs = form.querySelectorAll('input')
+
+allInputs.forEach(element =>{
+if (element.type === 'submit')return
+
+element.classList.add('input')
+element.addEventListener('input', () =>{
+        //console.log (element.validity)
+
+        const {valid} = element.validity
+        element.classList.toggle('invalid', !valid)
+
+        submitButton.disabled = !valid || !form.checkValidity()
+    })
+})
+
+const submitButton = form.querySelector('#submit')
+submitButton.disabled = true
+
+form.addEventListener('submit', e =>{
+    const data = [... new FormData(form)] 
+    localStorage.setItem('test', JSON.stringify(data))
+   e.preventDefault()
+} )
